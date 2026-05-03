e85e0e207b0668d1942672a445cfef984e020b17
Aggelos Tselios <aggelostselios777@gmail.com>
2026-05-03 12:54:18 +0300

    libc: stdint: Define `int_leastN_t` types
    Mandated by the C standard, so they should be present for applications
    to use.
    
    Signed-off-by: Aggelos Tselios <aggelostselios777@gmail.com>

--- libc/include/stdint.h
+++ libc/include/stdint.h
@@ -49,4 +49,14 @@ typedef signed long long int   intptr_t;
 typedef unsigned long long int uintptr_t;
 #endif /* __i386__ */
 
+typedef uint8_t  uint_least8_t;
+typedef uint16_t uint_least16_t;
+typedef uint32_t uint_least32_t;
+typedef uint64_t uint_least64_t;
+
+typedef int8_t  int_least8_t;
+typedef int16_t int_least16_t;
+typedef int32_t int_least32_t;
+typedef int64_t int_least64_t;
+
 DLC_END_DECLS
