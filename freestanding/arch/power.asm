b6ca42752d9220f9e5d963f4432edde451a53526
Aggelos Tselios <aggelostselios777@gmail.com>
2026-05-05 23:20:57 +0300

    freestanding: arch: Perform CPU reset using 8042 controller commands
    Triple faults cause a nasty warning in VirtualBox, because the virtual
    machine thinks the OS crashed and it wasn't intended. Additionally
    loading garbage into registers is hackish and in some cases may actually
    not triple fault but simply raise a CPU exception and the bootloader/OS
    will catch it and throw a FatalError().
    
    The PS/2 controller actually has the ability to perform a CPU reset:
    https://wiki.osdev.org/I8042_PS/2_Controller#CPU_Reset
    
    And since the controller is present even in the most modern machines,
    it can be used to perform a force reboot without needing an ugly triple
    fault.
    
    Signed-off-by: Aggelos Tselios <aggelostselios777@gmail.com>

--- freestanding/arch/power.asm
+++ freestanding/arch/power.asm
@@ -7,15 +7,28 @@
 ; LICENSE: GPL-v3.0-or-later, see COPYING in the toplevel directory
 ; ----------------------------------------------------------------------------------------
 
+PS2_PORT_STATUS		equ	0x64	; Controller status port
+PS2_PORT_COMMAND	equ	0x64	; Yes they're the same actually one's for input one's for output
+PS2_RESET_CPU_CMD	equ	0xFE	; Sending this byte to the PS/2 controller causes a CPU reset
+					; (Apparently it was a very powerful controller so they added all sorts
+					; of random shit into it)
 section .text
 global ForceReboot
 global StallMachine
 
+; https://wiki.osdev.org/I8042_PS/2_Controller#CPU_Reset
 ForceReboot:
-        lidt [0x4124814]
-        lgdt [0x1492112]
-        sti
-        jmp ForceReboot
+; The input buffer must be empty before we submit the command,
+; otherwise the reset fails.
+.wait:
+	in	al,	PS2_PORT_STATUS
+	test	al,	0x02
+	jne	.wait
+
+	mov	al,	PS2_RESET_CPU_CMD	
+	out	PS2_PORT_COMMAND,	al
+
+        jmp ForceReboot	; Failsafe, if the machine somehow survives the reset.
 
 StallMachine:
         cli
