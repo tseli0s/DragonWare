512fec8cfaa58138283dfb11a5968f846773f30b
Aggelos Tselios <aggelostselios777@gmail.com>
2026-06-08 18:39:30 +0300

    bootmanager: Add some more verbose logs in bootloader stage 2
    Might help with debugging headless things. No DragonWare can't run on
    anything headless probably but why not?
    
    Signed-off-by: Aggelos Tselios <aggelostselios777@gmail.com>

--- bootmanager/core/init/main.c
+++ bootmanager/core/init/main.c
@@ -320,6 +320,7 @@ void bootmain(void) {
                 AddEntry("Boot DragonWare (Default hard drive, video mode)", 2,
                          BootDragonWareDefaultOptions);
         }
+        AddEntry("Boot DragonWare (Default hard drive, text mode)", 1, BootDragonWareVGATextMode);
         AddEntry("Reboot", 3, ForceReboot);
 
         DrawUserInterface();
