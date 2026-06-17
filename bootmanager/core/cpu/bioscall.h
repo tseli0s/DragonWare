5e3eb3835fb5c0ab485e2c630f982d9fdb68d056
Aggelos Tselios <aggelostselios777@gmail.com>
2026-06-17 19:35:19 +0300

    boot: bioscall: Drop `SEGMENT_OF` and `OFFSET_IN_SEGMENT_OF` macros
    For whatever reason I cannot explain, they are breaking the bootloader
    entirely, when attempting to use them with a function I wrote. I am too
    tired to investigate, so I am just going to drop them and figure it out
    another day.
    
    Signed-off-by: Aggelos Tselios <aggelostselios777@gmail.com>

--- bootmanager/core/cpu/bioscall.h
+++ bootmanager/core/cpu/bioscall.h
@@ -11,19 +11,11 @@
 
 #include <ktypes.h>
 
-/*
- * Needed in real mode, where we have segmented addressing. Briefly, the way to convert a seg:offset
- * address to a linear address is (seg * 16) + offset. These two macros reverse that process by
- * taking a linear address and calculating the segment it is part of and what is the offset.
- */
-
-#define SEGMENT_OF(symbol)           (u16)(((uintptr_t)&(symbol) >> 4U) & 0xFFFFU)
-#define OFFSET_IN_SEGMENT_OF(symbol) (u16)((uintptr_t)&(symbol) & 0x000FU)
-
 /**
- * @brief Takes a segmented address (seg:offset scheme) and converts it into a linear 32 bit address.
- * @param[in] addr The address to convert. The format must be as following: High 16 bits are the segment of the address,
- * low 16 bits are the offset within that segment.
+ * @brief Takes a segmented address (seg:offset scheme) and converts it into a linear 32 bit
+ * address.
+ * @param[in] addr The address to convert. The format must be as following: High 16 bits are the
+ * segment of the address, low 16 bits are the offset within that segment.
  * @returns The linear address calculated from @p addr
  */
 static inline uintptr_t SegmentedToLinearAddress(u32 addr) {
@@ -32,9 +24,9 @@ static inline uintptr_t SegmentedToLinearAddress(u32 addr) {
         return (hi * 16) + lo;
 }
 
-#define BIOS_VIDEO_SERVICES          (0x10)
-#define BIOS_STORAGE_SERVICES        (0x13)
-#define BIOS_MEMORY_SERVICES         (0x15)
+#define BIOS_VIDEO_SERVICES   (0x10)
+#define BIOS_STORAGE_SERVICES (0x13)
+#define BIOS_MEMORY_SERVICES  (0x15)
 
 /**
  * @brief Register state to be loaded when performing a @ref BIOSCall
