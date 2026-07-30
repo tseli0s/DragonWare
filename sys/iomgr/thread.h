1f80b4a63fc5a8399477347b24a176f35f3a5619
Aggelos Tselios <aggelostselios777@gmail.com>
2026-07-31 00:52:48 +0300

    sys: iomgr: Implement thread object support
    Part 1/2. We need to implement thread deletion and blocking as well. But
    everything else is ready to go. It is now -theoretically, barely tested
    it- possible to create multithreaded applications in DragonWare, and
    there's ongoing work to improve upon this further.
    
    Signed-off-by: Aggelos Tselios <aggelostselios777@gmail.com>

---
+++ sys/iomgr/thread.h
@@ -0,0 +1,44 @@
+/**********************************************************************
+ * FILE: thread.h
+ * PURPOSE: Thread object related exports
+ * PROJECT: DragonWare Kernel
+ * DATE: 07-2026
+ * AUTHOR: Aggelos Tselios <aggelostselios777@gmail.com>
+ * LICENSE: GPL-3.0-or-later (https://spdx.org/licenses/GPL-3.0-or-later.html)
+ ***********************************************************************/
+
+#pragma once
+
+#include <ktypes.h>
+
+#include "iomgr/object.h"
+
+/**
+ * @brief Data describing thread initial state for use with thread objects.
+ * @since v0.0.2
+ */
+typedef struct [[gnu::packed]] _UserThreadData {
+        void *entry; /** << Entry point of a thread  */
+        void *stack; /** << Stack memory for the thread*/
+        void *extra_data; /** << Extra data that will be given to the thread upon execution.*/
+} UserThreadData;
+
+/**
+ * @brief Create a new thread object, setting up the @p stack for it and, when scheduled, begin
+ * execution in @p entryaddr for it.
+ *
+ * @param[in] entryaddr Thread entry point. Must be a pointer to a function where execution will
+ * begin.
+ * @param[in] stack Address of the stack to load for the thread. Cannot be a @ref NullPointer
+ * @note The resulting constructed thread is NOT put in the scheduler list.
+ * @returns A thread object as internal data for a @ref Object, or @ref NullPointer on failure.
+ */
+[[gnu::nonnull]]
+void *CreateThread(void (*entryaddr)(void), void *stack);
+
+/**
+ * @brief Appends @p thread to the scheduler list, preparing it for execution.
+ * @param[in] thread The thread to use. Must not be a @ref NullPointer
+ */
+[[gnu::nonnull]]
+void RunThread(Object *thread);
