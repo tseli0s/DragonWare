b7c7945112e6aedc6fa4496a6b47b028ff1d429e
Aggelos Tselios <aggelostselios777@gmail.com>
2026-08-01 21:34:55 +0300

    tree: Link flat binaries to 1MB (0x100000) base by default
    0x1000 is too low. Sure, it works, but I'd like to leave the lower 1MB
    alone so that we can do direct memory maps on the lower half if needed
    (like for legacy drivers) without conflicting with the servers.
    
    This is a breaking change, technically, and all applications will need
    to be recompiled, but who the fuck cares? I'm literally talking to
    myself in these commits.
    
    Signed-off-by: Aggelos Tselios <aggelostselios777@gmail.com>

--- sys/task/process.c
+++ sys/task/process.c
@@ -37,7 +37,7 @@
 /* ---------------------------------------------------------------------------------------------*/
 
 /* Where do flat binaries expect execution to start (In other words, where their origin is set) */
-#define FLAT_BINARY_DEFAULT_ENTRY ((ThreadEntryPoint)(0x1000))
+#define FLAT_BINARY_DEFAULT_ENTRY ((ThreadEntryPoint)(0x100000))
 
 /* Beginning of all kernel stacks in virtual memory. */
 #define KERNEL_STACK_BASE         (0xE0000000)
