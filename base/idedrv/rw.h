6c52f9748a182f9cfc349aa2d8d3188b48d7933e
Aggelos Tselios <aggelostselios777@gmail.com>
2026-09-07 13:40:46 +0300

    base: idedrv: Declare `WriteToDisk()` function
    Not implemented as of this commit, just a simple declaration while I am
    working on implementing it.
    
    Signed-off-by: Aggelos Tselios <aggelostselios777@gmail.com>

--- base/idedrv/rw.h
+++ base/idedrv/rw.h
@@ -30,3 +30,24 @@
 [[gnu::nonnull]]
 IDEDRVStatusReply ReadFromDisk(Handle irq_handle, IRQBindingDescriptor irq_descr, int bus,
                                int master, u32 lba, void *buf);
+
+/**
+ * @brief Write the provided 512 byte block into the LBA @p lba of the given drive @p master on bus
+ * @p bus.
+ * @param irq_handle Handle to the IRQ dispatch port where the kernel will notify of IRQ14/15
+ * arriving.
+ * @param irq_descr The @ref IRQBindingDescriptor associated with @p handle to ACK the IRQ
+ * @param bus 0 for primary bus, 1 for secondary bus.
+ * @param master 0 for master drive, 1 for slave drive.
+ * @param lba LBA to write the data to. High four bits are ignored.
+ * @param[in] buf The data to write to the disk. Must contain 512 bytes of data that will be written
+ * into the LBA @p lba of the selected drive. (See note below)
+ * @note If less than 512 bytes of data needs to be written into LBA @p lba (eg. Saving only 10
+ * bytes of changes in a single file), currently you must read the LBA @p lba into memory first,
+ * replace those 10 bytes and then write back the @p lba. This will be replaced in the future with a
+ * better API.
+ * @returns An appropriate @ref IDEDRVStatusReply code for the operation.
+ */
+[[gnu::nonnull]]
+IDEDRVStatusReply WriteToDisk(Handle irq_handle, IRQBindingDescriptor irq_descr, int bus,
+                              int master, u32 lba, void *buf);
